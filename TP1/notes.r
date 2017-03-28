3bf9e445c938fcb108adb1c1d92689d189cb12f7
ZS <zineb@pc56.home>
2017-03-29 00:38:10 +0200

    PCA exo 1 et 2 + crabs

--- TP1/notes.r
+++ TP1/notes.r
@@ -1,4 +1,8 @@
-#**********************Libraries****************
+#*****************************#
+
+#		   Libraries           #
+
+#******************************#
 library(MASS)
 library(gridExtra)
 library(Hmisc)
@@ -7,6 +11,11 @@ library(reshape2)
 library(grid)
 library(GGally) #ggpairs
 
+#*************************************************************************#
+
+#		   						Notes SY02          					  #
+
+#*************************************************************************#
 
 #*****************************#
 
@@ -14,23 +23,23 @@ library(GGally) #ggpairs
 
 #******************************#
 
-dataset = read.csv("data/sy02-p2016.csv", na.strings="", header=T)
-summary(dataset)
-describe(dataset) #This is more exhaustive than summary
+notes = read.csv("data/sy02-p2016.csv", na.strings="", header=T)
+summary(notes)
+describe(notes) #This is more exhaustive than summary
 notes$nom      = factor(notes$nom, levels=notes$nom)
 notes$niveau   = factor(notes$niveau, ordered=T)
 notes$resultat = factor(notes$resultat, levels=c("F","Fx","E","D","C","B","A"),ordered=T)
 
-n = nrow(dataset)
-p = ncol(dataset)
+n = nrow(notes)
+p = ncol(notes)
 sprintf("Number of individuals= %d",n)
 sprintf("Number of Variables= %d",p)
 
-any(is.na(dataset$resultat))
+any(is.na(notes$resultat))
 
-plot(dataset[2:11]) 
+plot(notes[2:11]) 
 
-data = data.frame(dataset$note.median, dataset$note.final, dataset$note.totale)
+data = data.frame(notes$note.median, notes$note.final, notes$note.totale)
 ggplot(melt(data), aes(variable, value, fill=variable)) + geom_boxplot() + scale_fill_discrete(name="Notes")
 
 
@@ -40,15 +49,34 @@ ggplot(melt(data), aes(variable, value, fill=variable)) + geom_boxplot() + scale
 
 #******************************#
 
-ggplot(dataset, aes(x= resultat, fill= dernier.diplome.obtenu))+ geom_bar(position="dodge")
-ggplot(dataset, aes(x= resultat, fill= niveau))+geom_bar(position="dodge")
-ggplot(dataset, aes(x= resultat, fill= specialite))+ geom_bar(position="dodge")
+ggplot(notes, aes(x= resultat, fill= dernier.diplome.obtenu))+ geom_bar(position="dodge")
+ggplot(notes, aes(x= resultat, fill= niveau))+geom_bar(position="dodge")
+ggplot(notes, aes(x= resultat, fill= specialite))+ geom_bar(position="dodge")
 
 
 vplayout = function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
 grid.newpage()
 pushViewport(viewport(layout = grid.layout(1, 2)))
-plot1 = ggplot(dataset, aes(x= correcteur.median, y= note.median ))+ geom_point()
-plot2 = ggplot(dataset, aes(x= correcteur.final, y= note.final))+ geom_point()
+plot1 = ggplot(notes, aes(x= correcteur.median, y= note.median ))+ geom_point()
+plot2 = ggplot(notes, aes(x= correcteur.final, y= note.final))+ geom_point()
 print(plot1, vp = vplayout(1, 1))
 print(plot2, vp = vplayout(1, 2))
+
+
+
+#*************************************************************************#
+
+#		   						Crabs          				         	  #
+
+#*************************************************************************#
+
+
+
+
+
+#*************************************************************************#
+
+#		   						Pima          				         	  #
+
+#*************************************************************************#
+
